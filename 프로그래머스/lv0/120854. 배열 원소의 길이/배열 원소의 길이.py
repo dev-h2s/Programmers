e02e947f90381d3fc9e6a42476718ab89f41ec17
dev-h2s <129861768+dev-h2s@users.noreply.github.com>
2023-07-02 15:39:57 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -0,0 +1,9 @@
+def solution(strlist):
+    return [len(s) for s in strlist]
+
+# def solution(strlist):
+#     result = []
+#     for s in strlist:
+#         length = len(s)
+#         result.append(length)
+#     return result
