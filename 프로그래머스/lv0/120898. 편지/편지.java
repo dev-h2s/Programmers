05750de248276887636345ad7ae77268e26b321f
dev-h2s <129861768+dev-h2s@users.noreply.github.com>
2023-08-01 17:39:49 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+//         문자열의 길이를 잰다
+        int length = message.length();
+         answer = length*2;
+        
+        return answer;
+    }
+}
